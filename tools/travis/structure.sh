a17aef0da7e0c1b4524d0c646e5e63597fc4c852
Erlend Klakegg Bergheim <erlend@klakegg.net>
2017-12-19 11:35:13 +0100

    Append rules for structure.

--- tools/travis/structure.sh
+++ tools/travis/structure.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v $(pwd)/structure/ubl-invoice:/src difi/vefa-structure:0.4.1
+docker run --rm -it -v $(pwd):/src -v $(pwd)/target/ubl-invoice:/target difi/vefa-structure:0.4.1 -p /src/structure/ubl-invoice -t /target
